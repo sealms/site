d89ca2b3dc90b13a6cb7d60e93b8acae841e9f14
Tama <thamously@gmail.com>
2020-10-16 20:29:30 +0700

    add contact & about

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import SEO from "../components/seo/seo"
+import Layout from "../components/layout/layout"
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" description="We are a small team with a big ❤️" />
+    <h1>About</h1>
+  </Layout>
+)
+
+export default AboutPage
